918364ddb4f4cbe0c788153af571dbc5dd86aed4
XiaoXuanyao <2664252025@qq.com>
2025-06-08 22:57:45 +0800

    Implement external network access

--- DigitalRecognitionWebsites/lib.js
+++ DigitalRecognitionWebsites/lib.js
@@ -1,7 +1,7 @@
 function requireService(sname, mes, respFunc) {
     // 创建 XMLHttpRequest 对象
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:25565/?" + sname, true);
+    xhr.open("POST", "http://hlchen200628.qicp.net/?" + sname, true);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.setRequestHeader("sname", sname);
     let mesStr = JSON.stringify(mes);
@@ -12,14 +12,6 @@ function requireService(sname, mes, respFunc) {
             respFunc(response);
         }
     };
-}
-
-async function sha256(message) {
-    const msgBuffer = new TextEncoder().encode(message); // 文本转字节
-    const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer); // 哈希
-    const hashArray = Array.from(new Uint8Array(hashBuffer)); // 转为数组
-    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join(""); // 转十六进制
-    return hashHex;
 }
 
 function onHeadLoad() {
